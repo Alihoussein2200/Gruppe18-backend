597aab062529faff97f91849a9e11073536c7c4e
daotzen98 <91252675+daotzen98@users.noreply.github.com>
2023-03-12 15:50:54 +0100

    City.tsx

---
+++ src/models/City.tsx
@@ -0,0 +1,10 @@
+export class City {
+    postalCode: string;
+    name: string;
+
+    constructor(postalCode: string, name: string){
+        this.postalCode = postalCode;
+        this.name = name;
+
+    }
+}
