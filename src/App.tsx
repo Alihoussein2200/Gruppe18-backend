9b54c73740563f9acc5aa3749b43b4db5164ec06
PeterM0ller <s215707@dtu.dk>
2023-05-01 01:37:59 +0200

    Loading dot i CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,47 @@ import { About } from "./pages/OmPage";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import { CheckoutPage } from "./pages/KassePage";
-import "./App.css"
 import { PaymentPage } from "./pages/BetalingPage";
+import { DotLoader } from "react-spinners";
+import React from "react";
+import "./App.css"
+
 
 function App() {
+
+  const [isLoading, setIsLoading] = React.useState(true);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+
   return (
-    <ShoppingCartProvider>
-      <Navbar />
-      <div className="AppContainer">
-        <Routes>
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/StorePreview" element={<CheckoutPage />} />
-          <Route path="/PaymentPage" element={<PaymentPage/>} />
-        </Routes>
-      </div>
-    </ShoppingCartProvider>
+    <div>
+      {isLoading && (
+        <div className="loadingDot">
+          <DotLoader color="#000" loading={isLoading} size={50} />
+        </div>
+      )}
+
+      <ShoppingCartProvider>
+        <Navbar />
+        <div className="AppContainer">
+          {isLoading ? null : (
+            <Routes>
+              <Route path="/store" element={<Store />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/StorePreview" element={<CheckoutPage />} />
+              <Route path="/PaymentPage" element={<PaymentPage />} />
+            </Routes>
+          )}
+        </div>
+      </ShoppingCartProvider>
+    </div>
   );
 }
 
 export default App;
+
+
