32ac3487b915af95494b1cf96a0c297d39b04547
PeterM0ller <s215707@dtu.dk>
2023-04-30 16:51:24 +0200

    Fjernet bootsrap fra app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
 import { Store } from "./pages/ShopPage";
 import { About } from "./pages/OmPage";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import { CheckoutPage } from "./pages/KassePage";
+import "./App.css"
 
 function App() {
   return (
     <ShoppingCartProvider>
-      <Navbar />
-      <Container className=" mb-4">
+      <Navbar/>
+      <div className="AppContainer">
         <Routes>
           <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
           <Route path="/StorePreview" element={<CheckoutPage />} />
         </Routes>
-      </Container>
+      </div>
     </ShoppingCartProvider>
   );
 }
