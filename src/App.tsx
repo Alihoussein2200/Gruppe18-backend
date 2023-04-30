82a631a994c69fa3519a7a9ecfe11c3be88f97a5
Alihoussein2200 <55292437+Alihoussein2200@users.noreply.github.com>
2023-04-30 14:07:13 +0200

    Added Loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,40 @@ import { About } from "./pages/OmPage";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import { CheckoutPage } from "./pages/KassePage";
+import { DotLoader } from "react-spinners";
+import React from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // Simulating loading time
+  }, []);
+
   return (
-    <ShoppingCartProvider>
-      <Navbar />
-      <Container className=" mb-4">
-        <Routes>
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/StorePreview" element={<CheckoutPage />} />
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
+    <>
+      {isLoading ? (
+        <div className="d-flex justify-content-center align-items-center h-100" style={{ marginTop: '100px' }}>
+        <DotLoader color="#000" loading={isLoading} size={50} />
+      </div>
+      
+      ) : (
+        <ShoppingCartProvider>
+          <Navbar />
+          <Container className=" mb-4">
+            <Routes>
+              <Route path="/store" element={<Store />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/StorePreview" element={<CheckoutPage />} />
+            </Routes>
+          </Container>
+        </ShoppingCartProvider>
+      )}
+    </>
   );
 }
 
 export default App;
+
