c4729bd0655a7280c1666facbaff4a02490c3e5a
Baime0 <s215790@dtu.dk>
2023-05-01 16:14:25 +0200

    changes fix

--- src/pages/BetalingPage.tsx
+++ src/pages/BetalingPage.tsx
@@ -1,15 +1,47 @@
 import { Link } from "react-router-dom";
 import "./BetalingPage.css";
 
+import { useEffect, useState } from "react";
+import { formatCurrency } from "../utilities/formatCurrency";
+import "./BetalingPage.css";
+
+import { CartItem } from "../components/CartItem";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+
 export function PaymentPage() {
   const handlePurchase = () => {
     alert("Tak fordi du handlede i Super Bazar Lyngby!");
   };
 
+  const [total, setTotal] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const { cartItems } = useShoppingCart();
+
+  const calculateTotal = () => {
+    const totalPrice = cartItems.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
+    setTotal(totalPrice);
+  };
+
+  function calculateDiscount() {
+    if (total > 300 && cartItems.length > 0) {
+      setDiscount(Math.round(total * 0.1));
+    } else {
+      setDiscount(0);
+    }
+  }
+
+  useEffect(() => {
+    calculateTotal();
+    calculateDiscount();
+  }, [cartItems, total]);
+
   return (
     <div className="creditCardForm">
       <div className="heading">
-        <h1>Godkendt Køb</h1>
+        <h1>kundeinformation</h1>
       </div>
       <div className="payment">
         <form>
@@ -22,22 +54,22 @@ export function PaymentPage() {
             <input type="text" className="form-control" id="cvv" />
           </div>
           <div className="form-group" id="card-number-field">
-            <label htmlFor="cardNumber">Kort Nummer</label>
+            <label htmlFor="cardNumber">Kortnummer</label>
             <input type="text" className="form-control" id="cardNumber" />
           </div>
           <div className="form-group" id="expiration-date">
-            <label>Udløbs dato</label>
+            <label>Udløbsdato</label>
             <select>
-              <option value="01">January</option>
-              <option value="02">February </option>
-              <option value="03">March</option>
+              <option value="01">Januar</option>
+              <option value="02">Februar </option>
+              <option value="03">Marts</option>
               <option value="04">April</option>
-              <option value="05">May</option>
-              <option value="06">June</option>
-              <option value="07">July</option>
+              <option value="05">Maj</option>
+              <option value="06">Juni</option>
+              <option value="07">Juli</option>
               <option value="08">August</option>
               <option value="09">September</option>
-              <option value="10">October</option>
+              <option value="10">Oktober</option>
               <option value="11">November</option>
               <option value="12">December</option>
             </select>
@@ -53,14 +85,14 @@ export function PaymentPage() {
           <div className="form-group" id="credit_cards"></div>
           <div className="form-group" id="pay-now">
             <Link to="/store">
-            <button
-              type="submit"
-              className="btn btn-default"
-              id="confirm-purchase"
-              onClick={handlePurchase}
-            >
-              Køb nu
-            </button>
+              <button
+                type="submit"
+                className="btn btn-default"
+                id="confirm-purchase"
+                onClick={handlePurchase}
+              >
+                Betal {formatCurrency(total - discount)}
+              </button>
             </Link>
           </div>
         </form>
