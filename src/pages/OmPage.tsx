91c000b11e9eea3d92dcab2725af12f18122f218
PeterM0ller <s215707@dtu.dk>
2023-05-01 14:17:29 +0200

    lidt små styling ændringer

--- src/pages/OmPage.tsx
+++ src/pages/OmPage.tsx
@@ -1,5 +1,8 @@
 export function About() {
-  return (<><h4> Webshop lavet af Gruppe 12 </h4>
-    <p>Lavet af Emmanuel, Ali, David, Ahmed og Peter</p></>
+  return (
+  <div>
+    <h4> Webshop lavet af Gruppe 12 </h4>
+    <p>Lavet af Emmanuel, Ali, David, Ahmed og Peter</p>
+  </div>
   );
 }
