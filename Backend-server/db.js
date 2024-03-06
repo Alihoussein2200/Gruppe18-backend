cb0540a9d4e0a3b1ce5f9e9210d974d9dce8cfe9
alishanoof <Alihoussein2200@gmail.com>
2024-03-06 11:50:55 +0100

    backend node js

---
+++ Backend-server/db.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const dbURI = process.env.DB_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+module.exports = mongoose;
